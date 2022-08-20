30703e3ebfb6750da75af68fdb5ad2a2889e8ee6
Georgiy Lelikov <jet_biker@mail.ru>
2022-08-20 15:52:54 +0300

    DZ 1.4

---
+++ DZ1.4/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на
+// выходе показывает все чётные числа от 1 до N.
+
+//Читерское решение этой задачи =)
+// Console.Write("Введите число: ");
+// int numberN = Convert.ToInt32(Console.ReadLine());
+// int number = 2;
+
+// while(number > 1 && number <= numberN)
+// {
+//     Console.Write(number + "; ");
+//     number = number + 2;
+// }
+
+// Универсальное решение
+Console.Write("Введите число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int number = 2;
+
+while(number > 1 && number <= numberN)
+{
+    int rem = number % 2;
+
+    if(rem == 0)
+    {
+        Console.Write(number + "; ");
+    }
+    
+    number++;
+}
