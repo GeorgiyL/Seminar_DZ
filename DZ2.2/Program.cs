504f106deaee7b01972125d47308c546f387228c
Georgiy Lelikov <jet_biker@mail.ru>
2022-08-25 16:16:52 +0300

    DZ2.2

---
+++ DZ2.2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+if(100 < num && num <1000)
+{
+    int num1 = num%10;
+    Console.WriteLine(num1);
+}
+if(1000 < num && num <10000)
+{
+    int num1 = num%100;
+    int num2 = num1/10;
+    Console.WriteLine(num2);
+}
+if(10000 < num && num <100000)
+{
+    int num1 = num%1000;
+    int num2 = num1/100;
+    Console.WriteLine(num2);
+}
