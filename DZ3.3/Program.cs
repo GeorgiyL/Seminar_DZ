6cd328542a4db54c2c3d7164a6b2c0e76429569c
Georgiy Lelikov <jet_biker@mail.ru>
2022-09-02 15:36:08 +0300

    DZ3.3

---
+++ DZ3.3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу, которая принимает на вход число (N) и выдает таблицу кубов чисел от 1 до N
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+// int num = 1; // Первый способ через цикл
+// while(num<=N)
+// {
+//     Console.Write(Math.Pow(num,3)+"; ");
+//     num++;
+// }
+
+for (int num=1; num<=N; num++) // Второй способ через for
+{
+    Console.Write(Math.Pow(num,3)+"; ");
+}
