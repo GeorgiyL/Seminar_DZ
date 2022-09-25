3e13cf6f61691546bbb304d26a162001f61cf354
Georgiy Lelikov <jet_biker@mail.ru>
2022-09-25 13:27:30 +0300

    DZ 9

---
+++ DZ9.1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string NaturalNum (int m, int n)
+{
+    if(m<=n) return $"{m} "+ NaturalNum(m+1,n);
+    else return string.Empty;
+}
+Console.WriteLine(NaturalNum(m,n));
---
+++ DZ9.2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int SumNum (int m, int n)
+{
+    if(m==n) return n;
+    return (m+SumNum(m+1,n));
+}
+Console.WriteLine(SumNum(m,n));
